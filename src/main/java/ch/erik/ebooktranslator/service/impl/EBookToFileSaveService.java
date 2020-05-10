aa55f07fdddef7008cc6ffcdc45e4564c21d3300
erik <erik.sattelmair@gmx.de>
2020-05-10 15:59:05 +0200

    Bugfix result writer

--- src/main/java/ch/erik/ebooktranslator/service/impl/EBookToFileSaveService.java
+++ src/main/java/ch/erik/ebooktranslator/service/impl/EBookToFileSaveService.java
@@ -4,21 +4,22 @@ import ch.erik.ebooktranslator.service.EBookSaveService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
 @Service
 @Slf4j
 public class EBookToFileSaveService implements EBookSaveService {
 
-    private static final String DEST_FOLDER_PATH = "";
+    private static final String DEST_FOLDER_PATH = "/Users/erik/Downloads";
 
     @Override
     public void saveBook(final byte[] eBook) throws IOException {
-        if(eBook != null) {
+        if (eBook == null) {
             throw new IllegalArgumentException("eBook must not be null!");
         }
 
-        Files.write(new File(DEST_FOLDER_PATH + "_" + System.currentTimeMillis() + ".epub").toPath(), eBook);
+        Files.write(new File(DEST_FOLDER_PATH + "translated_book_" + System.currentTimeMillis() + ".epub").toPath(), eBook);
     }
 }
