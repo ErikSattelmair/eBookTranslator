57e375692883f9c2cca9c4be9a4bd83d900cf5b6
erik <erik.sattelmair@gmx.de>
2020-05-12 08:35:25 +0200

    Better error handling

--- src/main/java/ch/erik/ebooktranslator/controller/TranslationController.java
+++ src/main/java/ch/erik/ebooktranslator/controller/TranslationController.java
@@ -1,6 +1,5 @@
 package ch.erik.ebooktranslator.controller;
 
-import ch.erik.ebooktranslator.model.TranslationProcessingModel;
 import ch.erik.ebooktranslator.model.TranslationRequestModel;
 import ch.erik.ebooktranslator.service.translation.WorkflowEngine;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -31,25 +32,37 @@ public class TranslationController {
                                final BindingResult bindingResult,
                                final Model model) {
         if (bindingResult.hasErrors()) {
-            model.addAttribute("requestInvalid", true);
+            final List<ObjectError> errors = bindingResult.getAllErrors();
+
+            for (final ObjectError error : errors) {
+                final String[] errorCodes = error.getCodes();
+
+                if (errorCodes != null) {
+                    final String relevantErrorCode = errorCodes[errorCodes.length - 1];
+
+                    if (relevantErrorCode.equalsIgnoreCase("coverImageFilePath")) {
+                        model.addAttribute("coverImageFilePathInvalid", true);
+                    }
+
+                    if (relevantErrorCode.equalsIgnoreCase("ebookFilePath")) {
+                        model.addAttribute("ebookFilePathInvalid", true);
+                    }
+                }
+            }
+
+            return "index";
+        } else {
+            final boolean translationSuccessful = this.workflowEngine.startWorkflow(translationRequestModel);
+
+            if (!translationSuccessful) {
+                model.addAttribute("translationSuccessful", false);
+                return "index";
+            }
+
+            model.addAttribute("translationSuccessful", true);
+
             return "index";
         }
-
-        final boolean translationSuccessful = this.workflowEngine.startWorkflow(translationRequestModel);
-        if (!translationSuccessful) {
-            model.addAttribute("translationSuccessful", false);
-            return "index";
-        }
-
-        model.addAttribute("translationSuccessful", true);
-
-        return "index";
-    }
-
-    @GetMapping("/processing-tool")
-    public String showTranslationProcessingPage(final Model model) {
-        model.addAttribute("translationProcessingModel", new TranslationProcessingModel());
-        return "processing-tool";
     }
 
 }
